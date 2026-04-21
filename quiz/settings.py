bdfa8adabe0885ac8bb1706e71699a7953e5ac25
abishek <abishekmagar222@gmail.com>
2026-04-21 09:35:41 +0545

    added Profile

--- quiz/settings.py
+++ quiz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-prd#*+@&3als1_pophbjzk_+xt63okvn7sjpn19qz#7z663wo5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quiz.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
